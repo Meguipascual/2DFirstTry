2a06d3a333b9d39005bd2c3b47c2245a912a3940
Meguipascual <meguipascual@gmail.com>
2022-09-26 03:51:53 +0200

    Some functions added.

--- 2DFirstTry/Assets/Scripts/BoardManager.cs
+++ 2DFirstTry/Assets/Scripts/BoardManager.cs
@@ -31,6 +31,47 @@ public class BoardManager : MonoBehaviour
     private Transform boardHolder;
     private List <Vector3> gridPositions = new List<Vector3>();
 
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+
+        for (int x = 0; x < columns - 1; x++)
+        {
+            for (int y = 0; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                {
+                    toInstantiate = outerWallTiles[Random.Range(0, wallTiles.Length)];
+                }
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPosition;
+    }
 
     // Start is called before the first frame update
     void Start()
