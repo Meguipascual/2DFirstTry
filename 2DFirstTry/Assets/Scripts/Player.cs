b1ced36f84b77b800bf40b2e006928574bfaece9
Meguipascual <meguipascual@gmail.com>
2022-09-28 21:38:34 +0200

    The "complete game" have been erased because of problems with scripts that had the same name. Text for each level and food added.

--- 2DFirstTry/Assets/Scripts/Player.cs
+++ 2DFirstTry/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MovingObject
 {
@@ -8,6 +10,7 @@ public class Player : MovingObject
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
+    public Text foodText;
 
     private Animator animator;
     private int food;
@@ -17,6 +20,7 @@ public class Player : MovingObject
     {
         animator = GetComponent<Animator>();
         food = GameManager.instance.playerFoodPoints;
+        foodText.text = "Food: "+ food;
         base.Start();
     }
 
@@ -50,10 +54,18 @@ public class Player : MovingObject
     protected override void AttemptMove<T>(int xDir, int yDir)
     {
         food--;
+        foodText.text = "Food: " + food;
         base.AttemptMove<T>(xDir, yDir);
         RaycastHit2D hit;
         CheckIfGameOver();
         GameManager.instance.playersTurn = false;
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("PlayerChop");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -65,30 +77,26 @@ public class Player : MovingObject
         }else if(other.tag == "Food")
         {
             food += pointsPerFood;
+            foodText.text = "+" + pointsPerFood + " Food: " + food; 
             other.gameObject.SetActive(false);
         }else if(other.tag == "Soda")
         {
             food += pointsPerSoda;
+            foodText.text = "+" + pointsPerFood + " Food: " + food;
             other.gameObject.SetActive(false);
         }
     }
 
-    protected override void OnCantMove<T>(T component)
-    {
-        Wall hitWall = component as Wall;
-        hitWall.DamageWall(wallDamage);
-        animator.SetTrigger("playerChop");
-    }
-
     private void Restart()
     {
-        Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void LoseFood(int loss)
     {
-        animator.SetTrigger("playerHit");
+        animator.SetTrigger("PlayerHit");
         food -= loss;
+        foodText.text = "-" + loss + " Food: " + food;
         CheckIfGameOver();
     }
 
